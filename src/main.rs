5d125cc9fc2c8c29c5fd5cb9ad18caa3606c34a0
Jochen Diepelt <jochen.diepelt@mailbox.tu-dresden.de>
2023-06-22 16:23:49 +0200

    adding alot a things for planning

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+
+// Einlesen eines Meshes
+// connectivität des Meshes
+
+// Connectivität zu Quad Elementen
+
+// Assemblierung Elemente
+
+// Assemblierung Randbedingungen
+
+// Penalty Verfahren für festgesetzte Randbedingungen.
+
+// Lösen
+
+// Rückwärtseinsetzen
+
+// Visualisierung
+
+fn main() {
+    println!("Hello, world!");
+}
