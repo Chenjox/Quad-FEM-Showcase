14b1ef885b888a98ba2f7de0a0bb3744386476ea
Koishi <david.diepelt@gmx.net>
2024-01-24 18:41:46 +0100

    adding vtu export

--- src/main.rs
+++ src/main.rs
@@ -179,8 +179,9 @@ impl WeakForm for Elasticity {
             result[(2, 1)] = shape_derivatives[(real_node, 0)]; // N1x
             result
         };
-
+        
         let result = b_mat_i.transpose() * self.material_matrix() * b_mat_j;
+        println!("{}",result);
 
         let mut resulting = OMatrix::<f64, Dyn, Dyn>::zeros(2, 2);
         for i in 0..(2 * 2) {
@@ -273,6 +274,8 @@ where
             let determinant =
                 jacobian[(0, 0)] * jacobian[(1, 1)] - jacobian[(0, 1)] * jacobian[(1, 0)];
 
+            println!("{}",jacobian);
+
             // Transformation auf tatsächliche Elemente
             let derivatives = (inv_jacobian * derivatives.transpose()).transpose();
 
@@ -308,8 +311,6 @@ where
                 }
             }
         }
-        //println!("{:3.3}", k);
-
         // Rechte Seite Vektor:
 
         // Wenn das Element tatsächlich einen Rand besitzt
@@ -394,7 +395,7 @@ where
             let edge_orientation = edge_index_orientation[1];
 
             let boundary_marker = mesh.boundary_type[(0, *boundary_element_index)];
-            println!("{}", boundary_marker);
+            //println!("{}", boundary_marker);
             // Globale Knotennummern (auch orientiert)
             let global_edge_nodes = mesh.boundary_elements.column(*boundary_element_index);
             // Lokale Knotennummern
@@ -451,7 +452,7 @@ fn main() {
 
     let elast = Elasticity {
         youngs_modulus: 1.0,
-        poissons_ratio: 0.2,
+        poissons_ratio: 0.0,
     };
 
     let traction = ConstantTractionForces {
@@ -544,7 +545,31 @@ fn main() {
     //println!("{:3.3}", dense_stiffness);
     //println!("{}", reduced_rhs);
 
+    let b = dense_stiffness.full_piv_lu();
+    let k = b.solve(&reduced_rhs).unwrap();
+
+    println!("{}",k);
+
+    // jetzt die Rückwärtsrolle
+    let mut correct_rhs = OVector::<f64,Dyn>::zeros(num_dofs);
+
+    let mut offset_counter = 0;
+    for i in 0..num_dofs {
+        if constraint_marker.contains(&i) {
+            correct_rhs[i] = 0.0;
+            offset_counter += 1;
+
+        } else {
+            correct_rhs[i] = k[i-offset_counter];
+        }
+    }
+
+
+    write_vkt_from_mesh(m, correct_rhs)
+
     // CG Verfahren
+
+    /*
 
     let mut precon = reduced_stiffness.diagonal_as_csr();
     precon.triplet_iter_mut().for_each(|f| *f.2 = 1.0 / (*f.2));
@@ -577,8 +602,7 @@ fn main() {
             break;
         }
     }
-
-    println!("{}",start_vec);
+    */
 
     // Dirichlet Rand
     /*
@@ -590,144 +614,73 @@ fn main() {
     */
 }
 
-/*
-fn main2() {
-    let m = get_FE_mesh(30.0, 3.0, 30, 10);
-    write_elements_to_file(&m);
 
-    let solution_dim = 1;
-    let mut global_matrix = OMatrix::<f64, Dyn, Dyn>::zeros(
-        solution_dim * m.points.ncols(),
-        solution_dim * m.points.ncols(),
+fn write_vkt_from_mesh(mesh: FEMesh<2>, solution: OMatrix<f64,Dyn,Const<1>>) {
+
+    let path = PathBuf::from(r"\test\test.vtu");
+
+    let mut points_vec = Vec::new();
+    for point in mesh.coordinates.column_iter() {
+        points_vec.push(point[0]);
+        points_vec.push(point[1]);
+        points_vec.push(0.0);
+    }
+
+    let num_elems = mesh.elements.ncols();
+    let mut connectivity_vec = Vec::new();
+    let mut offset_vec = Vec::new();
+
+    for elems in mesh.elements.column_iter() {
+        for vertex in elems.row_iter() {
+            connectivity_vec.push(vertex[0] as u64);
+        }
+        offset_vec.push(connectivity_vec.len() as u64);
+    }
+
+    let mut result_vector = Vec::new();
+    for (index,result) in solution.row_iter().enumerate() {
+        result_vector.push(result[0]);
+        if index % 2 == 1 {
+            result_vector.push(0.0)
+        }
+    }
+
+    println!("{:?}",result_vector);
+
+    let sols = Attribute::DataArray(
+        DataArray {
+            name: String::from("Displacement"),
+            elem: vtkio::model::ElementType::Vectors,
+            data: vtkio::IOBuffer::F64(result_vector)
+        }
     );
-    //println!("{:?}", global_matrix.shape());
 
-    // Hier beginnt die Assemblierung der Steifigkeitsmatrix
-    // Für jedes Element im Mesh
-    for (elem_index, elem) in m.elements.column_iter().enumerate() {
-        //println!("{}",elem);
-        //println!("Elem: {}", elem_index);
-        // Gauss-Regel,
-        let gauss = get_gauss_rule(2);
-
-        // Gauss-Points
-        //Elementsteifigkeit
-        let mut k = OMatrix::<f64, Const<4>, Const<4>>::zeros();
-        for gauss_point in gauss.column_iter() {
-            let xi_1 = gauss_point[0];
-            let xi_2 = gauss_point[1];
-            let weight = gauss_point[2];
-            //println!("Gauss: {:2.2},{:2.2}", xi_1, xi_2);
-
-            // Alle Elementkoordinaten des Geometrischen Elements
-            let shape_deriv_1 = g2_dxi1_vec(xi_1, xi_2);
-            let shape_deriv_2 = g2_dxi2_vec(xi_1, xi_2);
-
-            let mut jacobian: Matrix2x2 = Matrix2x2::zeros();
-            for (local_index, point_index) in elem.iter().enumerate() {
-            let point_index = *point_index;
-            let coords = m.get_point(point_index);
-
-                let gradient = OMatrix::<f64, Const<2>, Const<1>>::new(
-                    shape_deriv_1[local_index],
-                    shape_deriv_2[local_index],
-                );
-                let loc_jacobian = coords * gradient.transpose();
-
-                jacobian = jacobian + loc_jacobian;
-            }
-            let jacobian = jacobian;
-            let inv_jacobian = jacobian.try_inverse().unwrap();
-            //println!("{:2.2},{:2.2},{}", jacobian, inv_jacobian,jacobian.determinant());
-
-            // Jetzt alles des Tatsächlichen Elements
-
-            // Hier brauchen wir die Elementsteifigkeitsmatrix!
-
-            for (local_index_virt, point_index_virt) in elem.iter().enumerate() {
-                for (local_index, point_index) in elem.iter().enumerate() {
-                    //let point_index = *point_index;
-                    //let coords = m.get_point(point_index);
-
-                    let shape_deriv =
-                        inv_jacobian * shape_functions_2d_grad(local_index, xi_1, xi_2);
-
-                    let test_deriv =
-                        inv_jacobian * shape_functions_2d_grad(local_index_virt, xi_1, xi_2);
-
-                    let value = weight * shape_deriv.dot(&test_deriv);
-
-                    k[(local_index, local_index_virt)] += value;
-
-                    //println!("{:2.2},{:2.2}", point_index,point_index_virt);
-
-                    // So könnte man globale Koordinaten errechnen, aber das brauchen wir garnicht, da alles in lokalen ausgedrückt ist.
-                    //let global_coordinate = coords * shape_geom[local_index];
-                    //println!("{}",global_coordinate);
+    let vt = Vtk {
+        version: Version::new((0, 1)),
+        title: String::from("Heatmap"),
+        file_path: Some(path),
+        byte_order: vtkio::model::ByteOrder::LittleEndian,
+        data: DataSet::inline(
+            UnstructuredGridPiece {
+                points: points_vec.into(),
+                cells: Cells {
+                    cell_verts: VertexNumbers::XML { 
+                        offsets: offset_vec, 
+                        connectivity: connectivity_vec, 
+                    },
+                    types: vec![
+                        vec![CellType::Quad; num_elems]
+                    ].into_iter().flatten().collect::<Vec<CellType>>()
+                },
+                data: Attributes {
+                    point: vec![sols],
+                    cell: vec![]
                 }
             }
-        }
-        // Den Teil hier könnnte man sich sparen, wenn man das einpackt in ne größere Routine
-        for (local_p, global_p) in elem.iter().enumerate() {
-            for (local_q, global_q) in elem.iter().enumerate() {
-                global_matrix[(*global_p, *global_q)] += k[(local_p, local_q)]
-            }
-        }
-    }
-    //println!("{:2.4}", global_matrix);
+        ).into()
+    };
 
-    let A_cop = global_matrix.clone();
-    let A_cop_inv = A_cop.try_inverse().unwrap();
-    // Inhomogen Dirichlet
-    let mut projection = OMatrix::<f64, Dyn, Const<1>>::zeros(solution_dim * m.points.ncols());
-    let mut constrained =
-        OMatrix::<bool, Dyn, Const<1>>::from_element(solution_dim * m.points.ncols(), false);
+    let mut f = File::create("test.vtu").unwrap();
 
-    // Hier wird jetzt der Rand hardgecoded
-    for i in m.bounds.bottom.iter() {
-        projection[*i] = 100.;
-        //constrained[*i] = true;
-    }
-    for i in m.bounds.top.iter() {
-        projection[*i] = 0.;
-        constrained[*i] = true;
-    }
-    let mut rhs: OMatrix<f64, Dyn, Const<1>> =
-        OMatrix::<f64, Dyn, Const<1>>::zeros(m.points.ncols());
-
-    for i in 0..m.points.ncols() {
-        if constrained[i] {
-            for j in 0..m.points.ncols() {
-                global_matrix[(i, j)] = 0.;
-                global_matrix[(j, i)] = 0.;
-            }
-            global_matrix[(i, i)] = 1.;
-        }
-        rhs[i] = projection[i];
-    }
-
-    //println!("{:2.4}", global_matrix);
-    // Solving the system
-
-    // Hier landen die Unbekannten
-    //let x_vec = OMatrix::<f64,Dyn,Const<1>>::zeros(solution_dim*m.points.ncols());
-
-    let A = global_matrix;
-    let b = rhs;
-
-    //println!("{:2.4}",b);
-
-    let chol = A.cholesky().unwrap();
-    let inv_a = chol.inverse();
-
-    //let x = inv_a * b;
-    let x = chol.solve(&b);
-
-    //let sol = A_cop * x;
-
-    //println!("{}",x);
-
-    write_vkt_from_mesh(m, x);
+    vt.write_xml(&mut f).unwrap();
 }
-
-*/
